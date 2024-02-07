f18c0356a1116b0eafaf503ab81a7061887be7ef
vovka <no@mail.plz>
2024-02-08 01:21:41 +0300

    1707344501

---
+++ keys/apt-repo/regen.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+exit
+
+function exitinfo() {
+  echo $1
+  exit 1
+}
+
+export GNUPGHOME=$(mktemp -d /tmp/repoXXXXXX) && \
+  rm -rf $GNUPGHOME && \
+  mkdir $GNUPGHOME && \
+  chmod 700 $GNUPGHOME || exitinfo "Cannot create temp folder: $GNUPGHOME"
+
+result=""
+gpg --quick-generate-key apt-repo-key ed25519 sign never && \
+  gpg --armor --export-secret-keys apt-repo-key > ./apt-repo.private && \
+  gpg --export apt-repo-key > ./apt-repo.public && \
+  result="true"
+
+[ -n "$result" ] && gpg --list-keys
+
+gpgconf --kill all
+rm -rf $GNUPGHOME
+unset GNUPGHOME
+
+[ -z "$result" ] && exitinfo "Cannot generate or export keys"
+
+echo "###########"
+echo "NEXT:"
+echo "  Rebuild my-repo package to update public key"
+echo "  AND"
+echo "  Rebuild and upload apt repository"
+
+exit 0
