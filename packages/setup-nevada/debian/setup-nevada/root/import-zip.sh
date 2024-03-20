2b730150a6725a18d9899c0e8c57b0d853396fef
vovka <no@mail.plz>
2024-03-21 00:35:30 +0300

    1710970530

--- packages/setup-nevada/debian/setup-nevada/root/import-zip.sh
+++ packages/setup-nevada/debian/setup-nevada/root/import-zip.sh
@@ -11,9 +11,11 @@ fi
 tmp_dir=$(mktemp -d /tmp/impXXXXXX) && \
   chmod 700 "$tmp_dir" && \
   unzip "$import_file" -d "$tmp_dir" && \
-  rm -f "${tmp_dir}/ta.key" && \
+  cp -f "$tmp_dir"/* /etc/openvpn/pki-montana && \
+  cp -f "${tmp_dir}/ca.crt" /etc/nginx/pki-montana && \
   mv -f "${tmp_dir}/ca.crt" /etc/swanctl/x509ca && \
   mv -f "${tmp_dir}/"*.crt /etc/swanctl/x509 && \
+  rm -f "${tmp_dir}/ta.key" && \
   mv -f "${tmp_dir}/"*.key /etc/swanctl/private
 
 rm -rf "$tmp_dir"
